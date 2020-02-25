352db646ea07f3f3395825b5fc3780f3ccdafb45
Derick Gross <derick.gross@gmail.com>
2020-02-25 12:08:38 -0500

    testing commit to feature branch

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -18,6 +18,41 @@ class Api::BoardsController < ApplicationController
     render 'api/shared/error', status: :unprocessable_entity
   end
 
+  def show
+    return {
+            "id": 1,
+            "title": "Web dev",
+            "created_at": "2017-10-04T05:57:02.777Z",
+            "updated_at": "2017-10-04T05:57:02.777Z",
+            "lists": [
+              {
+                "id": 3,
+                "title": "CSS",
+                "board_id": 1,
+                "created_at": "2017-10-04T06:53:39.302Z",
+                "updated_at": "2017-10-04T06:53:39.302Z",
+                "position": 65535.0,
+                "cards": [
+                  {
+                    "id": 7,
+                    "title": "1",
+                    "due_date": null,
+                    "labels": [
+                      "red",
+                      "purple"
+                    ],
+                    "description": "Selectors",
+                    "list_id": 3,
+                    "board_id": 1,
+                    "position": 65535.0,
+                    "comments_count": 0
+                  }
+                ]
+              }
+            ]
+          }
+  end
+
   private
 
   def board_params

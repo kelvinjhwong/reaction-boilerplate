303d733bc806c06b10f55a418bf64f8ee12d11a8
Derick Gross <derick.gross@gmail.com>
2020-02-25 15:47:34 -0500

    added action types, board actions and client methods to support fetching a board

--- app/javascript/lib/ApiClient.js
+++ app/javascript/lib/ApiClient.js
@@ -26,6 +26,13 @@ const apiClient = {
       .then(callback)
       .catch(logError);
   },
+  getBoard: function(id, callback) {
+    return axios
+      .get(`${routs.BOARDS_INDEX_URL}${id}`)
+      .then(unwrapData)
+      .then(callback)
+      .catch(logError);
+  },
   createBoard: function(board, callback) {
     return axios
       .post(routes.CREATE_BOARD_URL, { board })

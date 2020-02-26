b8e6f339cdcaf1f08e5481e17b0213f5e9d820e8
Kelvin Wong <kjhwong@gmail.com>
2020-02-26 20:06:35 +0000

    Update boards and lists reducers

--- app/javascript/reducers/boards.js
+++ app/javascript/reducers/boards.js
@@ -2,6 +2,13 @@ export default function boards(state = [], action) {
   switch (action.type) {
     case "FETCH_BOARDS_SUCCESS":
       return action.boards;
+    case "FETCH_BOARD_SUCCESS":
+      const { lists, ...boardWithoutLists } = action.board;
+      const excludedBoards = state.filter(
+        board => board.id !== action.board.id
+      );
+
+      return excludedBoards.concat(boardWithoutLists);
     case "CREATE_BOARD_SUCCESS":
       const newBoard = action.board;
       return state.concat(newBoard);
--- app/javascript/reducers/lists.js
+++ app/javascript/reducers/lists.js
@@ -1,7 +1,12 @@
 export default function lists(state = [], action) {
   switch (action.type) {
-    case "FETCH_BOARDS_SUCCESS":
-      return action.lists;
+    case "FETCH_BOARD_SUCCESS":
+      const { lists, ...boardWithoutList } = action.board;
+
+      return lists.map(list => {
+        const { cards, ...listWithoutCards } = list;
+        return listWithoutCards;
+      });
     case "CREATE_LIST_SUCCESS":
       const newList = action.list;
       return state.concat(newList);

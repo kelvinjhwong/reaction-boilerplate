c8d32b88091cda3a1af14c4d586b0211b1c67052
Kelvin Wong <kjhwong@gmail.com>
2020-02-28 17:13:11 +0000

    Update formatting with Prettier

--- app/javascript/components/card/Cards.jsx
+++ app/javascript/components/card/Cards.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import Card from "./Card";
+import React from 'react';
+import Card from './Card';
 
-const Cards = props => {
+const Cards = (props) => {
   return (
-    <div id="cards-container" data-id="list-1-cards">
-      {props.cards.map(card => (
+    <div id='cards-container' data-id='list-1-cards'>
+      {props.cards.map((card) => (
         <Card key={card.id} card={card} />
       ))}
     </div>

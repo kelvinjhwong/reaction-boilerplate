1f801025c7bb7958072503673fdcf7afe14470b0
Derick Gross <derick.gross@gmail.com>
2020-02-28 13:45:45 -0500

    updates per Srdjan

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,6 +1,7 @@
 class Api::ListsController < ApplicationController
   def create
-    @list = List.new(list_params)
+    @board = Board.find(params[:board_id])
+    @list = List.new(list_params.merge(board: @board))
 
     if @list.save
       render :create, status: :created
@@ -11,17 +12,24 @@ class Api::ListsController < ApplicationController
   rescue ActionController::ParameterMissing
     @error = "Invalid list data provided"
     render 'api/shared/error', status: :unprocessable_entity
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 
   def update
     begin
       @list = List.find_by(id: params[:id])
 
-      list_params[:board_id] ? @list.board_id = list_params[:board_id] : nil
-      list_params[:title] ? @list.title = list_params[:title] : nil
-      list_params[:position] ? @list.position = list_params[:position] : nil
+      # params[:board_id] ? @list.board_id = params[:board_id] : nil
+      # list_params[:title] ? @list.title = list_params[:title] : nil
+      # list_params[:position] ? @list.position = list_params[:position] : nil
 
-      @list.save
+      if (@list.update(list_params))
+        render :update
+      else
+        @error = @list.errors.full_messages.join(', ')
+        render 'api/shared/error', status: :unprocessable_entity
+      end
     rescue ActiveRecord::RecordNotFound
       render_404
     end
@@ -30,6 +38,6 @@ class Api::ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:board_id, :title, :position)
+    params.require(:list).permit(:title, :position)
   end
 end
